cb879f02fc15945b4548a92ab198043c0a179198
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-26 12:19:23 +0100

    chore: created template for scroll snippet

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HELLO PORTFOLIO</h1>
+      <section className="one">
+        <h1>First Page</h1>
+      </section>
+      <section className="two">
+        <h1>Second Page</h1>
+      </section>
+      <section className="three">
+        <h1>Third Page</h1>
+      </section>
+      <section className="four">
+        <h1>Fourth Page</h1>
+      </section>
+      <section className="five">
+        <h1>Fifth Page</h1>
+      </section>
     </div>
   );
 }
