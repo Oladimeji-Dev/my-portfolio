0268c4b9052c525e80e6b5e92abc6cf5dd40407f
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-26 18:58:21 +0100

    chore: added the homepage to the app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
+import { HomePage } from './pages/Homepage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <section className="one">
-        <h1>First Page</h1>
-      </section>
-      <section className="two">
-        <h1>Second Page</h1>
-      </section>
-      <section className="three">
-        <h1>Third Page</h1>
-      </section>
-      <section className="four">
-        <h1>Fourth Page</h1>
-      </section>
-      <section className="five">
-        <h1>Fifth Page</h1>
-      </section>
+      <HomePage />
     </div>
   );
 }
