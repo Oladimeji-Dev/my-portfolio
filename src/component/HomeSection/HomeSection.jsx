abd49785d29111b214248447e5ca93ea226c9df1
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-26 18:57:12 +0100

    chore: homesection page created

---
+++ src/component/HomeSection/HomeSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HomeSection.css'
+export const HomeSection = () => {
+  return (
+        <section className="one">
+            <h1>Home Page</h1>
+        </section>
+    )
+}
