f6d3b6cd33635b5c590bad46999ed27958d06e04
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-27 17:34:49 +0100

    chore: created the file for the skils

---
+++ skills.js
@@ -0,0 +1,39 @@
+
+export const skills = [
+    {
+        name : "HTML 5",
+        image : "../public/icons/html-logo.svg"
+    },
+    {
+        name : "CSS",
+        image : "../public/icons/css-logo-logo.svg"
+    },
+    {
+        name : "JAVASCRIPT ES6",
+        image : "../public/icons/js-logo.svg"
+    },
+    {
+        name : "BOOTSTRAP",
+        image : "../public/icons/bootstrap-logo.png"
+    },
+    {
+        name : "TAILWIND",
+        image : "../public/icons/tailwind-logo.svg"
+    },
+    {
+        name : "NPM",
+        image : "../public/icons/npm-logo.svg"
+    },
+    {
+        name : "REACT",
+        image : "../public/icons/react-logo.svg"
+    },
+    {
+        name : "GIT",
+        image : "../public/icons/git-logo.svg"
+    },
+    {
+        name : "TYPESCRIPT",
+        image : "../public/icons/typecript.svg"
+    },
+]
