590deda3d18db114036366b3fc5f74d454366df4
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-27 18:53:33 +0100

    chore: modifield the skills script file

---
+++ src/skills.js
@@ -0,0 +1,39 @@
+
+export const skills = [
+    {
+        name : "HTML5",
+        image : "icons/html-logo.svg"
+    },
+    {
+        name : "CSS",
+        image : "icons/css-logo.svg"
+    },
+    {
+        name : "JAVASCRIPT ES6",
+        image : "icons/js-logo.svg"
+    },
+    {
+        name : "BOOTSTRAP",
+        image : "icons/bootstrap-logo.png"
+    },
+    {
+        name : "TAILWIND",
+        image : "icons/tailwind-logo.svg"
+    },
+    {
+        name : "NPM",
+        image : "icons/npm-logo.svg"
+    },
+    {
+        name : "REACT",
+        image : "icons/react-logo.svg"
+    },
+    {
+        name : "GIT",
+        image : "icons/git-logo.svg"
+    },
+    {
+        name : "TYPESCRIPT",
+        image : "icons/typescript.svg"
+    },
+]
