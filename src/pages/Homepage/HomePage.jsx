7d4dbdb8cc7a587ddc85526a343a67f2991221df
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-27 14:22:07 +0100

    chore: working on the second section responsivenes

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -13,7 +13,9 @@ export const HomePage = () => {
             <Navbar fill='#5034d4'logo={logop} />
             <div className="about-me">
                 <div className="about-me-image">
-                    <img src={profile} alt="profile" />
+                    <span className='about-me-image-border'>
+                        <img src={profile} alt="profile" />
+                    </span>
                 </div>
                 <div className="about-me-dec">
                     <header className='about-me-dec-header'>
@@ -22,7 +24,7 @@ export const HomePage = () => {
                     </header>
                     <main className='about-me-dec-main'>
                         <p>
-                        Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
+                        Hello! My name is Abdulwasiu Oladimeji Saliman and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
                         </p>
                         <p>
                         Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
