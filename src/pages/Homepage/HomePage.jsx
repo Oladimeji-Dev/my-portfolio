bd2cd97f8a3590b835c3dd304f37d322a817f5ae
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-30 12:34:46 +0100

    chore: finished the html element for the fourth page

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -3,6 +3,7 @@ import './HomePage.css'
 import { HomeSection } from '../../component/HomeSection/HomeSection'
 import { Navbar } from '../../component/SecNavbar/Navbar'
 import logop from '../../assets/images/cube_002.png'
+import logol from '../../assets/images/cube_001.png'
 import profile from '../../assets/images/Profile.jpeg'
 import { skills } from '../../skills'
 // import { Link } from 'react-router-dom'
@@ -11,7 +12,7 @@ export const HomePage = () => {
   return (
     <div className='container'>
         <HomeSection />
-        <section className="two">
+        {/* <section className="two">
             <Navbar fill='#5034d4'logo={logop} />
             <div className="about-me">
                 <div className="about-me-image">
@@ -66,12 +67,31 @@ export const HomePage = () => {
                     </div>
                 </div>
             </div>
-        </section>
+        </section> */}
         <section className="four">
-            <h1>Fourth Page</h1>
+            <Navbar fill='#ccf381'logo={logol} />
+            <div className="send-message">
+                <header className='send-message-header'>
+                    <h1>Send me a message!</h1>
+                    <p>
+                        Got a question or proposal, or just want <br />
+                        to say hello? Go ahead.
+                    </p>
+                </header>
+                <form className="send-message-form">
+                    <div className="first-form">
+                        <input type="text" placeholder='Enter your name' />
+                        <input type="text" placeholder='Enter your email address' />
+                    </div>
+                    <div className="second-form">
+                        <textarea placeholder='Hi, I think we need a design system for our products at Company X. How soon can you hop on to discuss this?'></textarea>
+                    </div>
+                    <input className='btn' type="submit" value="SHOOT" />
+                </form>
+            </div>
         </section>
         <section className="five">
-            <h1>Fifth Page</h1>
+            <h1>Five page</h1>
         </section>
     </div>
   )
