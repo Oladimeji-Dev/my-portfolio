1a007c23579270449d2a1ae27a8385499961de28
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-02 16:17:42 +0100

    chore: created the Works Page

---
+++ src/pages/Works/Works.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Footer } from '../../component/Footer/Footer'
+import "./Works.css"
+export const Works = () => {
+  return (
+    <div className='Works'>
+        <h1>Works Page UnderConstruction</h1>
+        <Footer />
+    </div>
+  )
+}
