04dde3632aca0ff3a93c4ece47d96f7a4c453d58
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-02 15:37:12 +0100

    chore: removed code from the homepage

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,49 +1,17 @@
 import React from 'react'
 import './HomePage.css'
 import { HomeSection } from '../../component/HomeSection/HomeSection'
-import { Navbar } from '../../component/SecNavbar/Navbar'
-// import logop from '../../assets/images/cube_002.png'
-import logol from '../../assets/images/cube_001.png'
-import { Footer } from '../../component/Footer/Footer'
 import { AboutMe } from '../../component/AboutMe/AboutMe'
 import { TechnicalSkills } from '../../component/TechnicalSkills/TechnicalSkills'
-
+import { SendMessage } from '../../component/SendMessage/SendMessage'
+import { Footer } from '../../component/Footer/Footer'
 export const HomePage = () => {
   return (
     <div className='container'>
         <HomeSection />
         <AboutMe />
         <TechnicalSkills />
-        
-        <section className="four">
-            <Navbar fill='#ccf381'logo={logol} />
-            <div className="send-message">
-                <header className='send-message-header'>
-                    <h1>Send me a message! &#128231;</h1>
-                    <p>
-                        Got a question or proposal, or just want <br />
-                        to say hello? Go ahead.
-                    </p>
-                </header>
-                <form className="send-message-form">
-                    <div className="first-form">
-                        <div className="form-ne">
-                            <label htmlFor='name'>Your Name</label>
-                            <input type="text" id='name' placeholder='Enter your name' />
-                        </div>
-                        <div className="form-ne">
-                            <label htmlFor='email'>Email Address</label>
-                            <input type="text" id='email' placeholder='Enter your email address' />
-                        </div> 
-                    </div>
-                    <div className="second-form">
-                        <label htmlFor='message'>Your Message</label>
-                        <textarea id='message' rows="7" minLength="30" placeholder='Hi, I think we need a design system for our products at Company X. How soon can you hop on to discuss this?'></textarea>
-                    </div>
-                    <input className='btn btn-message' type="submit" value="SHOOT ----->" />
-                </form>
-            </div>
-        </section>
+        <SendMessage />
         <Footer />
     </div>
   )
