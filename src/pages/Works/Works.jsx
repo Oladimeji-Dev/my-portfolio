4b307896136e142687feb071698475fb47631fb1
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-02 20:08:56 +0100

    chore: worked on the card of the projects

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Footer } from '../../component/Footer/Footer'
 import { Navbar } from '../../component/Navbar/Navbar'
 import logop from '../../assets/images/002_txt.png'
+import gitIcon from '../../assets/icons/github.svg'
+import externalIcon from '../../assets/icons/external.svg'
+// import myPortImg from '../../assets/images/my-port-web.png'
 import "./Works.css"
 export const Works = () => {
   return (
@@ -9,19 +12,120 @@ export const Works = () => {
         <Navbar logo={logop} fill = '#4831d4' />
         <div className="works">
             <header className='works-header'>
-                <h3>/<span>work</span>.</h3>
+                <h1>
+                    /
+                    <span className='works-header-span'>works</span>
+                    .
+                </h1>
                 <p>Selected projects I've worked on in the past.</p>
             </header>
             <div className="works-projects">
                 <div className="project-card">
                     <div className="project-card-image">
-                        --image---
+                        <img src={logop} alt="portimg" />
                     </div>
                     <div className="project-card-details">
                         <p className='project-card-name'>
-                            project name
+                            Personal Website
                         </p>
-                        <a href="#1" target="_blank" rel="noopener noreferrer" >project link</a>
+                        <div className="project-card-links">
+                            <a href="https://github.com/Oladimeji-Dev/" target="_blank" rel="noopener noreferrer" >
+                                <img src={gitIcon}alt="gitimage" />
+                            </a>
+                            <a href="https://oladimeji-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer" >
+                                <img src={externalIcon}alt="exterimage" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <div className="project-card-image">
+                        <img src={logop} alt="portimg" />
+                    </div>
+                    <div className="project-card-details">
+                        <p className='project-card-name'>
+                            Personal Website
+                        </p>
+                        <div className="project-card-links">
+                            <a href="https://github.com/Oladimeji-Dev/" target="_blank" rel="noopener noreferrer" >
+                                <img src={gitIcon}alt="gitimage" />
+                            </a>
+                            <a href="https://oladimeji-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer" >
+                                <img src={externalIcon}alt="exterimage" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <div className="project-card-image">
+                        <img src={logop} alt="portimg" />
+                    </div>
+                    <div className="project-card-details">
+                        <p className='project-card-name'>
+                            Personal Website
+                        </p>
+                        <div className="project-card-links">
+                            <a href="https://github.com/Oladimeji-Dev/" target="_blank" rel="noopener noreferrer" >
+                                <img src={gitIcon}alt="gitimage" />
+                            </a>
+                            <a href="https://oladimeji-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer" >
+                                <img src={externalIcon}alt="exterimage" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <div className="project-card-image">
+                        <img src={logop} alt="portimg" />
+                    </div>
+                    <div className="project-card-details">
+                        <p className='project-card-name'>
+                            Personal Website
+                        </p>
+                        <div className="project-card-links">
+                            <a href="https://github.com/Oladimeji-Dev/" target="_blank" rel="noopener noreferrer" >
+                                <img src={gitIcon}alt="gitimage" />
+                            </a>
+                            <a href="https://oladimeji-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer" >
+                                <img src={externalIcon}alt="exterimage" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <div className="project-card-image">
+                        <img src={logop} alt="portimg" />
+                    </div>
+                    <div className="project-card-details">
+                        <p className='project-card-name'>
+                            Personal Website
+                        </p>
+                        <div className="project-card-links">
+                            <a href="https://github.com/Oladimeji-Dev/" target="_blank" rel="noopener noreferrer" >
+                                <img src={gitIcon}alt="gitimage" />
+                            </a>
+                            <a href="https://oladimeji-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer" >
+                                <img src={externalIcon}alt="exterimage" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <div className="project-card-image">
+                        <img src={logop} alt="portimg" />
+                    </div>
+                    <div className="project-card-details">
+                        <p className='project-card-name'>
+                            Personal Website
+                        </p>
+                        <div className="project-card-links">
+                            <a href="https://github.com/Oladimeji-Dev/" target="_blank" rel="noopener noreferrer" >
+                                <img src={gitIcon}alt="gitimage" />
+                            </a>
+                            <a href="https://oladimeji-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer" >
+                                <img src={externalIcon}alt="exterimage" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
