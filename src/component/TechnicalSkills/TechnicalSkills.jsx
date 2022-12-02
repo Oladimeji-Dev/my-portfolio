5084408b2f7f98c19e4f61e389bdbdc7d3b05607
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-02 15:26:06 +0100

    chore: added the TechnicalSkils

---
+++ src/component/TechnicalSkills/TechnicalSkills.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './TechnicalSkills.css'
+import logop from '../../assets/images/cube_002.png'
+import { skills } from '../../skills'
+import { Link } from 'react-router-dom'
+import { Navbar } from '../../component/SecNavbar/Navbar'
+export const TechnicalSkills = () => {
+  return (
+    <section className="three">
+        <Navbar fill='#5034d4'logo={logop} />
+        <div className="knowledge">
+            <div className="knowledge-projects">
+                <h1>
+                    I build & Program stuff
+                </h1>
+                <p>
+                    Open source projects, <br/>web apps and experimentals.
+                </p>
+                <Link to='/' className='btn btn-works'>SEE MY WORKS ----</Link>
+            </div>
+            <div className="knowledge-skills">
+                <h1>Technical Skills</h1>
+                <div className="technical-icons">
+                    {
+                        skills.map((skill,index) =>(
+                            <div key={index} className="skill-card">
+                                <img src={skill.image} alt='skill' />
+                                <p>
+                                    {skill.name}
+                                </p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
