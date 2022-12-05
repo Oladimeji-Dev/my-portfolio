bf51aae35aafe56f0c0eaafcfe2684f7bae1c2ab
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-05 23:23:56 +0100

    added the toggle and handletodo to homepage

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import './HomePage.css'
+import { useState } from 'react'
 import { HomeSection } from '../../component/HomeSection/HomeSection'
 import { AboutMe } from '../../component/AboutMe/AboutMe'
 import { TechnicalSkills } from '../../component/TechnicalSkills/TechnicalSkills'
 import { SendMessage } from '../../component/SendMessage/SendMessage'
 import { Footer } from '../../component/Footer/Footer'
 export const HomePage = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () =>{
+    setToggle((prevtoggle) => !prevtoggle)
+  }
   return (
     <div className='container'>
-        <HomeSection />
-        <AboutMe />
-        <TechnicalSkills />
-        <SendMessage />
-        <Footer shownav={true} />
+        <HomeSection toggle = {toggle} handleToggle = {handleToggle} />
+        <AboutMe toggle = {toggle} handleToggle = {handleToggle} />
+        <TechnicalSkills toggle = {toggle} handleToggle = {handleToggle} />
+        <SendMessage toggle = {toggle} handleToggle = {handleToggle} />
+        <Footer toggle = {toggle} handleToggle = {handleToggle} shownav={true} />
     </div>
   )
 }
