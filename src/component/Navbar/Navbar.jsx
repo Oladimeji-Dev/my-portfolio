0fcb3d41b6a1211f085ccc975966f9ea4df12d21
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-05 22:18:18 +0100

    feat: just build the toggle nav

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -12,11 +12,11 @@ export const Navbar = ({logo,fill}) => {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M16,30A14,14,0,1,1,30,16,14,14,0,0,1,16,30ZM16,4A12,12,0,1,0,28,16,12,12,0,0,0,16,4Zm7,6H9v2H23Zm0,5H9v2H23Zm0,5H9v2H23Z" fill={fill} data-name="15  Menu, Hamburger, Hamburger Menu"/></svg>
         </div>
         <div className="hambuger-nav">
-          <div className="hambuger-nav-container">
             <header>
               <img className='ham-nav-logo' src={hamlogo} alt="logo" />
               <img className='ham-nav-close' src={close} alt="close" />
             </header>
+          <div className="hambuger-nav-container">
             <ul className='hambuger-nav-links'>
               <li>
                 <Link to="/work">My Work</Link>  
