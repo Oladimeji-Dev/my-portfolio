469b0caa724ed867bc8f09164d5d86b0d76e6b15
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-05 22:31:32 +0100

    feat: added the toggle functionality

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import hamlogo from '../../assets/images/002_txt.png'
 import close from '../../assets/images/x.svg'
 
 export const Navbar = ({logo,fill}) => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () =>{
+    setToggle((prevtoggle) => !prevtoggle)
+  }
+  console.log(toggle)
   return (
     <div className='navbar'>
         <img src={logo} alt="logo" className='logo'/>
-        <div className="menu">
+        <div className={toggle ? 'menu-hide' : 'menu'} onClick={handleToggle}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M16,30A14,14,0,1,1,30,16,14,14,0,0,1,16,30ZM16,4A12,12,0,1,0,28,16,12,12,0,0,0,16,4Zm7,6H9v2H23Zm0,5H9v2H23Zm0,5H9v2H23Z" fill={fill} data-name="15  Menu, Hamburger, Hamburger Menu"/></svg>
         </div>
-        <div className="hambuger-nav">
+        <div className= {toggle ? "hambuger-nav" : "hambuger-nav menu-hide" }>
             <header>
               <img className='ham-nav-logo' src={hamlogo} alt="logo" />
-              <img className='ham-nav-close' src={close} alt="close" />
+              <img onClick={handleToggle} className='ham-nav-close' src={close} alt="close" />
             </header>
           <div className="hambuger-nav-container">
             <ul className='hambuger-nav-links'>
