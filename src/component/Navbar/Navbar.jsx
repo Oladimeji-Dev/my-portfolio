7bc1abe56c4c5fa4e8854a031abc4faa6c6ef113
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-05 21:12:51 +0100

    chore: aadded the menu hide

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -8,7 +8,9 @@ export const Navbar = ({logo,fill}) => {
   return (
     <div className='navbar'>
         <img src={logo} alt="logo" className='logo'/>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M16,30A14,14,0,1,1,30,16,14,14,0,0,1,16,30ZM16,4A12,12,0,1,0,28,16,12,12,0,0,0,16,4Zm7,6H9v2H23Zm0,5H9v2H23Zm0,5H9v2H23Z" fill={fill} data-name="15  Menu, Hamburger, Hamburger Menu"/></svg>
+        <div className="menu">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M16,30A14,14,0,1,1,30,16,14,14,0,0,1,16,30ZM16,4A12,12,0,1,0,28,16,12,12,0,0,0,16,4Zm7,6H9v2H23Zm0,5H9v2H23Zm0,5H9v2H23Z" fill={fill} data-name="15  Menu, Hamburger, Hamburger Menu"/></svg>
+        </div>
         <div className="hambuger-nav">
           <div className="hambuger-nav-container">
             <header>
@@ -23,8 +25,8 @@ export const Navbar = ({logo,fill}) => {
                 <a href="#2">My Resume</a>
               </li>
             </ul>
-            <p>SAY HELLO</p>
             <ul className='hambuger-nav-links'>
+              <p>SAY HELLO</p>
               <li>
                   <a href="#2">hello@Oladimeji.com</a>
               </li>
