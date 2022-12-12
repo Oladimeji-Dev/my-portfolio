d989d82878f85d16163162ef513c36bf0520fcc6
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-12-12 13:44:24 +0100

    chore: added the new not found route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import { HomePage } from './pages/Homepage/HomePage';
 import { Works } from './pages/Works/Works';
+import { NotFound } from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />}/>    
           <Route path='/works' element={<Works />}/>    
-          <Route path='*' element={<><h1>Page Not Found</h1></>}/>    
+          <Route path='*' element={<NotFound />}/>    
         </Routes>
       </BrowserRouter>
 
